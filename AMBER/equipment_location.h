5e26ec31552e1afeee2ea63bae48309bae70e0e8
Codehead <BitBucket@codehead.co.uk>
2014-04-21 20:24:03 +0100

    Start of AMBER ver 4

---
+++ AMBER/equipment_location.h
@@ -0,0 +1,14 @@
+#ifndef __EQUIPMENT_LOCATION_H__
+#define __EQUIPMENT_LOCATION_H__
+
+class equipment_location
+{
+	private:
+		char _name[16];
+		int _qty;
+
+	public:
+
+};
+
+#endif
---
+++ AMBER/hit_location.h
@@ -0,0 +1,26 @@
+#ifndef __HIT_LOCATION_H__
+#define __HIT_LOCATION_H__
+
+class hit_location
+{
+	private:
+
+	char _name[16];
+	int _structure;
+	int _front_armour;
+	int _rear_armour;
+	bool has_rear_armour;
+	equipment main_equipment[6];
+	equipment sec_equipment[6];
+	bool has_sec_equipment;
+
+	hit_location child_location;
+	hit_location parent_location;
+
+	public:
+
+
+
+};
+
+#endif
